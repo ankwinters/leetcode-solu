044d6b0578596597793752bcc5c096095c99646b
ckp <ckpwinters@gmail.com>
2015-03-29 15:00:18 +0800

    fix bug

--- 1359/xunhuan.py
+++ 1359/xunhuan.py
@@ -3,13 +3,16 @@
 #Just for fun.
 #You have been warned.
 if __name__=='__main__':
-    num_string=input("please enter two numbers:")
-    n,k=num_string.split(' ')
-    n=int(n)
-    k=int(k)
-    for x in range(2,10**600):
-        if n**x % (10**k)==n%(10**k):
-            print(x)
-            break
-    else:
-        print(1)
+    try:
+        num_string=input("please enter two numbers:")
+        n,k=num_string.split(' ')
+        n=int(n)
+        k=int(k)
+        for x in range(2,10**600):
+            if n**x % (10**k)==n%(10**k):
+                print(x)
+                break
+        else:
+            print(1)
+    except:
+        print("Please check your input!!!")
