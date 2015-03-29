b8f4a566120f6377af26412fcdfb9424951fb499
ckp <ckpwinters@gmail.com>
2015-03-29 12:02:18 +0800

    Violent traverse

---
+++ 1359/xunhuan.py
@@ -0,0 +1,11 @@
+#!/usr/bin python
+
+if __name__=='__main__':
+    num_string=input("please enter two numbers:")
+    n,k=num_string.split(' ')
+    n=int(n)
+    k=int(k)
+    for x in range(2,10**600):
+        if n**x % (10**k)==n%(10**k):
+            print(x)
+            break
