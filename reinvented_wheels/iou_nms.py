4daf53e40be0c737d7a3bbc654dad6049152c514
ckp <ckpwinters@gmail.com>
2018-10-07 20:58:00 +0800

    Fix:iou

--- reinvented_wheels/iou_nms.py
+++ reinvented_wheels/iou_nms.py
@@ -7,8 +7,8 @@ def compute_iou(box1, box2):
     w1, h1 = abs(x2-x1), abs(y2-y1)
     w2, h2 = abs(x4-x3), abs(y4-y3)
     # Intersection box
-    w_in = w1 + w2 - abs(x3-x1)
-    h_in = h1 + h2 - abs(y3-y1)
+    w_in = w1 + w2 - max(abs(x4-x1), abs(x3-x2))
+    h_in = h1 + h2 - max(abs(y4-y1), abs(y3-y2))
     if w_in > 0 and h_in > 0:
         return (w_in*h_in) / (w1*h1 + w2*h2 - w_in*h_in)
     else:
@@ -33,6 +33,7 @@ def nms(box_confi_list, thresh=0.5):
         for j in range(i+1, len(s_box_confi_list)):
             box2 = s_box_confi_list[j]
             iou = compute_iou(box[:-1], box2[:-1])
+
             # Suppress
             if iou > thresh:
                 s_box_confi_list[j] = ()
@@ -41,5 +42,5 @@ def nms(box_confi_list, thresh=0.5):
 
 
 if __name__ == '__main__':
-    l2 = [(2, 1, 3, 4, 0.8), (3, 4, 5, 6, 0.9)]
-    nms(l2, 0)
+    l2 = [(2, 4, 3, 1, 0.8), (2.2, 6, 5, 1.1, 0.9)]
+    print(nms(l2))
